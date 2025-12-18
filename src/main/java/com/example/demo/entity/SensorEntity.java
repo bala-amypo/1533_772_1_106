34a81912a1b2aaa51cd68d4e6f23e4c9a8ee2f0c
bala-amypo <bala@amypo.in>
2025-12-18 09:00:21 +0000

    User commit

--- src/main/java/com/example/demo/entity/SensorEntity.java
+++ src/main/java/com/example/demo/entity/SensorEntity.java
@@ -1,11 +1,55 @@
 package com.example.demo.entity;
 
-import jakarta.persistence.Column;
-import jakarta.peristance.Entity;
-
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity
-public class SensorEntity{
+public class SensorEntity {
+
     @Id
-    @GeneratedValue(strategy=Generation)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String sensorCode;
+
+    private String sensorType;
+
+    private Boolean isActive = true;
+
+    public SensorEntity() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getSensorCode() {
+        return sensorCode;
+    }
+
+    public void setSensorCode(String sensorCode) {
+        this.sensorCode = sensorCode;
+    }
+
+    public String getSensorType() {
+        return sensorType;
+    }
+
+    public void setSensorType(String sensorType) {
+        this.sensorType = sensorType;
+    }
+
+    public Boolean getIsActive() {
+        return isActive;
+    }
+
+    public void setIsActive(Boolean isActive) {
+        this.isActive = isActive;
+    }
 }
