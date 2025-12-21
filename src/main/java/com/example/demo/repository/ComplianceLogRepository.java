d97de35d1aa5ad3243359a1fb3f8b6003c48f9ec
bala-amypo <bala@amypo.in>
2025-12-21 17:36:18 +0000

    User commit

--- src/main/java/com/example/demo/repository/ComplianceLogRepository.java
+++ src/main/java/com/example/demo/repository/ComplianceLogRepository.java
@@ -1,15 +1,11 @@
 package com.example.demo.repository;
 
 import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import com.example.demo.entity.ComplianceLog;
 
 @Repository
 public interface ComplianceLogRepository extends JpaRepository<ComplianceLog, Long> {
-
-List<ComplianceLog> findBySensorReading_Id(Long id);
-
+    List<ComplianceLog> findBySensorReadingId(Long id);  // Use field name exactly
 }
