79f829e6f8cad2eb8f3e125f0459e9efc4c8168e
bala-amypo <bala@amypo.in>
2025-12-21 10:11:54 +0000

    User commit

---
+++ src/main/java/com/example/demo/entity/Location.java
@@ -0,0 +1,75 @@
+package com.example.demo.entity;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.UniqueConstraint;
+
+@Entity
+public class Location {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "location_name", nullable = false, unique = true)
+    private String locationName;
+
+    private String description;
+
+    @Column(nullable = false)
+    private String region;
+
+    private LocalDateTime createdAt;
+
+    public Location() {
+
+    }
+
+    public Location(String locationName, String description, String region, LocalDateTime createdAt) {
+        this.locationName = locationName;
+        this.description = description;
+        this.region = region;
+        this.createdAt = createdAt;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getLocationName() {
+        return locationName;
+    }
+
+    public void setLocationName(String locationName) {
+        this.locationName = locationName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+}
