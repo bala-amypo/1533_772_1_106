1f2caa6e8c0fd04e1f9aece2cdb796aa845b3a53
bala-amypo <bala@amypo.in>
2025-12-21 14:09:57 +0000

    User commit

--- src/main/java/com/example/demo/repository/SensorReadingRepository.java
+++ src/main/java/com/example/demo/repository/SensorReadingRepository.java
@@ -1,17 +1,13 @@
 package com.example.demo.repository;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.example.demo.entity.SensorReading;
 
-@Repository
-public interface SensorReadingRepository extends JpaRepository<SensorReading, Long> {
+public interface SensorReadingRepository
+        extends JpaRepository<SensorReading, Long> {
 
-    List<SensorReading> findBySensor_Id(Long sensorId);
-
-    List<SensorReading> findBySensor_IdAndReadingTimeBetween(Long sensorId, LocalDateTime start, LocalDateTime end);
+    List<SensorReading> findBySensorId(Long sensorId);
 }
