03ab7597c80479994b76ccfc3c836b1b0c07793b
bala-amypo <bala@amypo.in>
2025-12-22 00:13:21 +0000

    User commit

--- src/main/java/com/example/demo/entity/Location.java
+++ src/main/java/com/example/demo/entity/Location.java
@@ -7,14 +7,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import jakarta.persistence.UniqueConstraint;
 
 @Entity
-@Table(
-    name = "locations",
-    uniqueConstraints = {@UniqueConstraint(columnNames = "location_name")}
-)
+
 public class Location {
 
     @Id
