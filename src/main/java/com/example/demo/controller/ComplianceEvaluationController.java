6e34edf38fb81e613b84091795ae9397f773a750
bala-amypo <bala@amypo.in>
2025-12-25 10:30:38 +0000

    User commit

--- src/main/java/com/example/demo/controller/ComplianceEvaluationController.java
+++ src/main/java/com/example/demo/controller/ComplianceEvaluationController.java
@@ -1,4 +1,4 @@
-=package com.example.demo.controller;
+package com.example.demo.controller;
 
 import com.example.demo.entity.ComplianceLog;
 import com.example.demo.service.ComplianceEvaluationService;
